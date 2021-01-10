fefea12300f72657b8350fcaeec914aa64ed2e4d
meksociety <tanakorn.to@ku.th>
2021-01-10 14:35:06 +0700

    create file name about ตัวแปร

---
+++ variable/name.py
@@ -0,0 +1,17 @@
+#สามารถประกาศได้
+myvar = "MEK"
+my_var = "MEK"
+_my_var = "MEK"
+myVar = "MEK"
+MYVAR = "MEK"
+myvar2 = "MEK"
+
+#ไม่สามารถประกาศได้
+2myvar = "MEK"
+my-var = "MEK"
+my var = "MEK"
+
+#สำคัญ
+
+## ตัวแปรที่ใช้ตัวใหญ่จะตกลงกันว่าใช้เป็นค่าคงที่เช่น
+PI = 3.14
