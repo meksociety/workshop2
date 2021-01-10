962c682c7d411cfe96f6f4ecb2d38d8969160da2
meksociety <tanakorn.to@ku.th>
2021-01-10 14:23:06 +0700

    create comment in line 1 print hello world in line 2

---
+++ variable/variable.py
@@ -0,0 +1,18 @@
+name = "Tanakorn Toomthong"
+age = 24
+weight = 80.0
+# Boolen i can is has
+can_walk = True
+can_run = False
+
+print(name)
+print(age)
+print(weight)
+print(can_walk)
+print(can_run)
+
+print(type(name))
+print(type(age))
+print(type(weight))
+print(type(can_walk))
+print(type(can_run))
