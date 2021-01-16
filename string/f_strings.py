060a7b3674a30b28c6ea364f67448e3112d8b9cb
meksociety <tanakorn.to@ku.th>
2021-01-16 15:23:40 +0700

    add datatype , string : con, f_string , modifystring, slicing , stringformat

---
+++ string/f_strings.py
@@ -0,0 +1,6 @@
+name = "mek"
+age = 21
+
+result = f" My name is {name}, and I am {age} "
+
+print("result", result)  # Output : "My name is mek, and I am 21"
---
+++ string/stringformat.py
@@ -0,0 +1,6 @@
+age = 21
+txt = "My name is Mek, and I am {}"
+
+result = txt.format(age)
+
+print("result", result)  # Output: "My name is Mek, and I am 21"
