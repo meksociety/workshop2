8073c0f5fdff370f30299ff7cd2d5ee9bd2edbae
meksociety <tanakorn.to@ku.th>
2021-01-17 14:26:06 +0700

    add all in list

---
+++ list/changeitemvalue.py
@@ -0,0 +1,11 @@
+thislist = ["apple", "banana", "cherry"]
+thislist[1] = "watermelon"
+
+print(thislist)  # Output : ['apple', 'watermelon', 'cherry']
+
+###join####
+list1 = [1, 2, 3]
+list2 = [4, 5, 6]
+
+list3 = list1 + list2
+print(list3)  # Output : [1, 2, 3, 4, 5, 6]
