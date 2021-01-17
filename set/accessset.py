1cb16f51927cf7a6025d74438a90195018823965
meksociety <tanakorn.to@ku.th>
2021-01-17 15:17:03 +0700

    fix accesset

--- set/accessset.py
+++ set/accessset.py
@@ -2,8 +2,15 @@
 thisset = {"apple", "banana", "cherry"}
 for x in thisset:
     print(x)
+# OP
+# banana
+# cherry
+# apple
+
 
 # EX-2
 fruits = {"apple", "banana", "cherry"}
 if "apple" in fruits:
     print("Yes, apple is fruit!")
+# OP
+# Yes, apple is fruit!
