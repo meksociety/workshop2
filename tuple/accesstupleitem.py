c65465a994008ad2410d2a4f68419053372728a5
meksociety <tanakorn.to@ku.th>
2021-01-17 14:36:42 +0700

    add comment

--- tuple/accesstupleitem.py
+++ tuple/accesstupleitem.py
@@ -1,11 +1,11 @@
 thislist = ("apple", "banana", "cherry")
 
-print(thislist[0])
-print(thislist[1])
-print(thislist[2])
+print(thislist[0])  # OP apple
+print(thislist[1])  # OP banana
+print(thislist[2])  # OP cherry
 
-print(thislist[-1])
-print(thislist[-2])
-print(thislist[-3])
+print(thislist[-1])  # OP cherry
+print(thislist[-2])  # OP banana
+print(thislist[-3])  # OP apple
 
-print(thislist[1:])
+print(thislist[1:])  # OP ('banana', 'cherry')
