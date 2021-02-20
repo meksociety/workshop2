7ab85871310e39e61a4d3a14bb74fa811fa34406
meksociety <tanakorn.to@ku.th>
2021-02-20 15:14:39 +0700

    add module class function variable

---
+++ module/class/runclass.py
@@ -0,0 +1,12 @@
+import classPerson
+
+name = "Thanakorn"
+age = 21
+address = "Thailand"
+
+person = classPerson.Person(name, age, address)
+
+print(person.get_name())
+print(person.get_age())
+print(person.get_address())
+print(person.get_info())
---
+++ module/variable/variable.py
@@ -0,0 +1,2 @@
+person = {"name": "MEK", "age": 21, "country": "chonburi"}
+person1 = {"name": "POOPAE", "age": 20, "country": "chonburi"}
